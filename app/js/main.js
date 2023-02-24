455381f25364ba3a70f640dab61f4d990253cd11
Pivodur <megazyamba@gmail.com>
2023-02-24 21:39:10 +0600

    add responsive

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,23 @@ $(function(){
       ratedFill: "#000", 
       spacing: "14px",
       readOnly: true,
-    });
+   });
+
+   $('.burger').on('click',function(){
+      $('.header-top').toggleClass('header-top--active');
+   });
+
+   $('.cross').on('click',function(){
+      $('.header-top').toggleClass('header-top--active');
+   });
+
+   $('.header-top__menu-link').on('click',function(){
+      $('.header-top').toggleClass('header-top--active');
+   });
+   if (window.innerWidth <= 375) {
+      const spans = document.querySelectorAll('br:not([class])');
+      spans.forEach(span => span.remove());
+    }
+  
    
 });
