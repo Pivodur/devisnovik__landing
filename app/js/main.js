aa0bb3ae4d98889f304392652f2f506da43c77b8
Pivodur <megazyamba@gmail.com>
2023-02-24 15:59:44 +0600

    make desktop frame

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function(){
-
+   $(".star").rateYo({
+      starWidth: "20px",
+      normalFill: "#E5E5E5",
+      ratedFill: "#000", 
+      spacing: "14px",
+      readOnly: true,
+    });
+   
 });
